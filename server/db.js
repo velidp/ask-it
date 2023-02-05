b2ad4feee98aaeb3fc7a783c8e87b8ad2939ece4
Velid Posko <velid.posko@zira.com.ba>
2023-02-05 15:27:58 +0100

    Fix

--- server/db.js
+++ server/db.js
@@ -2,13 +2,13 @@ const Pool = require('pg').Pool;
 
 const pool = new Pool({
 
-      user: 'velidp',
-      host: 'ep-broad-forest-934451.eu-central-1.aws.neon.tech',
-      database: 'neondb',
-      password: 'pfWlLxkZ6G0e',
+      user: 'ask_it_user',
+      host: 'dpg-cffommo2i3mg6p9f3r70-a',
+      database: 'ask_it',
+      password: 'FtS5tsFxL64wi2uZGxAVfgd5lxPN1jMM',
       
 
-    connectionString: 'postgres://velidp:pfWlLxkZ6G0e@ep-broad-forest-934451.eu-central-1.aws.neon.tech/neondb',
+    connectionString: 'postgres://ask_it_user:FtS5tsFxL64wi2uZGxAVfgd5lxPN1jMM@dpg-cffommo2i3mg6p9f3r70-a.oregon-postgres.render.com/ask_it',
     ssl: {
       rejectUnauthorized: false
     }
